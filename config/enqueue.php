beb37d88c8b63ee267abb59b70198f1430162e0e
Kaiser Tushar <kaiser.tushar@yahoo.com>
2025-01-24 08:11:15 -0500

    update config/enqueue.php file to add example of asset load and data passing

--- config/enqueue.php
+++ config/enqueue.php
@@ -4,14 +4,14 @@ return [
     'admin' => [
         'scripts' => [
             [
-                'handle' => 'reviewx-admin-js', 'src' => 'assets/js/admin.js', 'deps' => [], 'ver' => false, 'in_footer' => false, 'attributes' =>
+                'handle' => 'reviewx-admin-js', 'src' => 'resources/assets/js/admin.js', 'deps' => [], 'ver' => false, 'in_footer' => false, 'attributes' =>
                 [
                     'async' => 'async',
                     'data-x' => 'test'
                 ],
             ],
             [
-                'handle' => 'reviewx-user-js', 'src' => 'assets/js/user.js', 'deps' => [], 'ver' => false, 'in_footer' => false, 'attributes' =>
+                'handle' => 'reviewx-user-js', 'src' => 'resources/assets/js/user.js', 'deps' => ['jquery'], 'ver' => false, 'in_footer' => false, 'attributes' =>
                 [
                     'async' => 'async',
                     'data-y' => 'tx'
@@ -19,21 +19,30 @@ return [
             ],
         ],
         'styles' => [
-            ['handle' => 'reviewx-admin-css', 'src' => 'assets/css/admin.css', 'deps' => [], 'ver' => false, 'media' => 'all', 'attributes' =>
+            ['handle' => 'reviewx-admin-css', 'src' => 'resources/assets/css/admin.css', 'deps' => [], 'ver' => false, 'media' => 'all', 'attributes' =>
                 [
                     'async' => 'async',
                     'data-y' => 'tx'
                 ],
             ],
-        ]
+        ],
+        'localize_scripts' => [
+            [
+                'handle' => 'reviewx-user-js',
+                'objectName' => 'user',
+                'data' => [
+                    'is_admin' => true,
+                ],
+            ],
+        ],
 
     ],
     'frontend' => [
         'scripts' => [
-            ['handle' => 'reviewx-admin-script', 'src' => 'assets/js/admin.js', 'deps' => [], 'ver' => false, 'in_footer' => true],
+            ['handle' => 'reviewx-admin-script', 'src' => 'resources/assets/js/admin.js', 'deps' => [], 'ver' => false, 'in_footer' => true],
         ],
         'styles' => [
-            ['handle' => 'reviewx-admin-style', 'src' => 'assets/css/admin.css', 'deps' => [], 'ver' => false, 'media' => 'all'],
+            ['handle' => 'reviewx-admin-style', 'src' => 'resources/assets/css/admin.css', 'deps' => [], 'ver' => false, 'media' => 'all'],
         ]
     ]
 ];
